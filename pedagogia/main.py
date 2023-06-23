8fef661da6a7948f5d25f0b00d5f84c13ce40d6f
gabriel <gabrielsbadenas@gmail.com>
2023-06-23 17:36:32 -0300

    sujetos

---
+++ pedagogia/main.py
@@ -0,0 +1,13 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v171)
+"   /home/g/Documentos
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../
+./
+apuntes-materias/
+godot/
+cv-gabriel-sanchez.txt
+langs.txt
